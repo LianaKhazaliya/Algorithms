a033d8f6ef4b4e49877462a3aa3c7010a6cf5d96
LianaKhazaliya <khazaliya.liana@gmail.com>
2019-04-08 00:21:44 +0300

    v1

--- Task4/gen.py
+++ Task4/gen.py
@@ -1,23 +1,22 @@
-u = 1
-w = 2
-x = 0.30277286414
-y = 3
-z = 0.00001
+from random import randint
+
+u = randint(0, 5)
+w = randint(0, 5)
+x = randint(0, 5)
+y = randint(0, 5)
+z = randint(0, 5)
+
 powU = [2, 0, 1, 1, 0]
 powW = [2, 1, 1, 2, 0]
 powX = [1, 2, 1, 1, 0]
 powY = [1, 1, 0, 1, 1]
 powZ = [2, 1, 2, 2, 0]
+
 Result = 34
 
 data = zip([u, w, x, y, z], [powU, powW, powX, powY, powZ])
-S = 0
-for i in range(5):
-	S += sum([el[0]**el[1][i] for el in data])
-
-print S
-
-
+print data
+print sum(sum(el[0]**el[1][i] for el in data) for i in range(5))-Result
 
 
 
