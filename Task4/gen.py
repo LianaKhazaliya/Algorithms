66460210d6609a5094536de3cf5d5a3cd5a22b0f
LianaKhazaliya <khazaliya.liana@gmail.com>
2019-04-14 18:26:10 +0300

    It works

--- Task4/gen.py
+++ Task4/gen.py
@@ -1,10 +1,12 @@
 from random import randint, random
+from pprint import pprint
+from operator import itemgetter
 
 MIN, MAX = -100, 100
 VARS = 5
-
+GEN_MAX=2
 MUT_MIN, MUT_MAX = -3, 3
-NGEN, CXPB, MUTPB, MUTGENPB = 10, 0.5, 0.1, 0.02
+NGEN, CXPB, MUTPB, MUTGENPB = 10, 0.5, 0.8, 0.8
 DEFAULT_MAIN_ARGS = NGEN, CXPB, MUTPB
 
 
@@ -23,11 +25,16 @@ def fitness(instance):
     return abs(sum(sum(el[0]**el[1][i] for el in data) for i in range(5))-Result)
 
 
+def fitness_calc(population):
+    return map(fitness, population)
+
+
 def mutate(instance):
-    if random.random() <= MUTPB:
-        for gen in instance:
+    if random() <= MUTPB:
+        for i in range(len(instance)):
             if random() <= MUTGENPB:
-                gen += randint(MUT_MIN, MUT_MAX)
+                instance[i] += randint(MUT_MIN, MUT_MAX)
+    return instance
 
 def mutation(population):
     return map(mutate, population)
@@ -38,14 +45,14 @@ def selection(population, popsize):
     for i in range(popsize):
         parents = []
         for j in range(3):
-            inst1 = population[randint(0, popnum-1)]
-            inst2 = population[randint(0, popnum-1)]
+            inst1 = population[randint(0, popsize-1)]
+            inst2 = population[randint(0, popsize-1)]
             if fitness(inst1) <= fitness(inst2):
                 parents.append(inst1)
             else:
                 parents.append(inst2)
 
-        parents_pairs += zip(parents, parents[1:]+parents[0])
+        parents_pairs += zip(parents, parents[1:]+parents[:1])
     return parents_pairs
 
 
@@ -55,58 +62,51 @@ def crossover(pair):
         parent1[2], parent2[2] = parent2[2], parent1[2]
     else:
         parent1[1:4], parent2[1:4] = parent2[1:4], parent1[1:4]
-    return parent1, parent2
+    return [parent1, parent2]
 
 
-def newpop_selection(population):
-    newpop = []
+def newpop_selection(population, popsize):
+    newpop_data = []
     curpop = sorted(zip(map(fitness, population), population))[::-1]
     newpop_data += curpop[:popsize/10]
-    while len(newpop) != popsize:
-        newpop_data += curpop[randint(3*popsize-1)]
+    while len(newpop_data) != popsize:
+        newpop_data.append(curpop[randint(0, 6*popsize-1)])
 
     newpop = [data[1] for data in newpop_data]
     return newpop
 
 
 def create_population(popsize, ngen):
-    return [(randint(MIN, MAX) for j in range(ngen))] for i in range(popsize)]
+    return [[randint(MIN, MAX) for j in range(ngen)] for i in range(popsize)]
 
-def fitness_calc(population):
-    return dict(zip(map(fitness, population), population)), dict(zip(population, map(fitness, population)))
 
-def is_zero(popdata_fi):
-    return 0 not in popdata_fi.keys()
+def is_zero(popdata_f):
+    return 0 not in popdata_f
 
 def is_nextgen(generation):
     return generation < GEN_MAX
 
-def is_continue(generation, popdata):
-    return is_zero and is_nextgen
+def is_continue(generation, popdata_f):
+    return is_zero(popdata_f) and is_nextgen(generation)
 
 
 def main(ngen, popsize):
     population = create_population(popsize, ngen)
-    popdata_fi, popdata_if = fitness_calc(population)
+    popdata_f = fitness_calc(population)
     generation = 0
 
-    while is_continue(generation, popdata):
+    while is_continue(generation, popdata_f):
+
         parents_pairs = selection(population, popsize)
-        childs = [crossover(pair) for pair in parents_pairs]
+        childs = []
+        for pair in parents_pairs:
+            childs += crossover(pair)
         mut_childs = mutation(childs)
-        population = newpop_selection(mut_childs)
-        popdata_fi,popdata_if = fitness_calc(population)
+        population = newpop_selection(mut_childs, popsize)
+        popdata_f = fitness_calc(population)
         generation += 1
 
-    if not is_zero(popdata):
-        print popdata_fi[0]
-
-    else:
-        print popdata_if[min(popdata_if.keys())]
-
+    print population[min(enumerate(popdata_f), key=itemgetter(1))[0]]
 
 if __name__ == '__main__':
-    population = create_population(10, 5)
-    print fitness_calc(population)
-
-
+    main(5,10)
